74b393d782ca8225191725b1c032450679601cf6
Fabian Cernota <fabian.cernota@freifunk-westerwald.de>
2016-07-17 19:12:36 +0200

    Add testnetz

---
+++ testnetz/site.mk
@@ -0,0 +1,64 @@
+##	gluon site.mk makefile for Freifunk Westerwald
+
+##	GLUON_SITE_PACKAGES
+#		specify gluon/openwrt packages to include here
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-core \
+	gluon-config-mode-autoupdater\
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged \
+	gluon-luci-private-wifi
+
+ifeq ($(GLUON_TARGET),x86-generic)
+# support the usb stack on x86 devices
+# and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-dm9601-ether
+endif
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
+DEFAULT_GLUON_RELEASE := unset
+
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
+
+
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
+
+# Languages to include
+GLUON_LANGS ?= de
