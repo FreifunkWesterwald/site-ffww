8043bf6acd39ef96563008c569b2fa23c0674f2f
justelex <github.acc@maik-nauheim.de>
2016-10-05 11:33:43 +0200

    Fix Shellsyntax

--- nixbuild.sh
+++ nixbuild.sh
@@ -2,31 +2,31 @@
 #! nix-shell -i /bin/bash -p openssl zlib ncurses gawk file gcc49 getopt git gnumake path perl pkgconfig python27 subversion unzip which bash expat pyxml perlPackages.XMLSAX glibc boost intltool libftdi libgcrypt bc binutils wget
 cd ${WORKSPACE}
 echo ${Westerwald}
-if [${Westerwald} = true];
+if ${Westerwald} ;
    then
       cp ./ffwwsites/westerwald ./site -r
       make update
       make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${Altenkirchen} = true];
+if ${Altenkirchen} ;
    then
       cp ./ffwwsites/altenkirchen ./site -r
       make update
       make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${Limburg} = true];
+if ${Limburg} ;
    then
       cp ./ffwwsites/limburg ./site -r
       make update
       make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${Neuwied} = true];
+if ${Neuwied} ;
    then
       cp ./ffwwsites/neuwied ./site -r
       make update
       make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${Testnetz} = true];
+if ${Testnetz} ;
    then
       cp ./ffwwsites/testnetz ./site -r
       make update
