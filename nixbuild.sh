4848da5c698e9414e97be95650d22a3c7686c0cb
justelex <github.acc@maik-nauheim.de>
2016-10-05 11:27:44 +0200

    Fix Shellsyntax

--- nixbuild.sh
+++ nixbuild.sh
@@ -1,28 +1,34 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i /bin/bash -p openssl zlib ncurses gawk file gcc49 getopt git gnumake path perl pkgconfig python27 subversion unzip which bash expat pyxml perlPackages.XMLSAX glibc boost intltool libftdi libgcrypt bc binutils wget
 cd ${WORKSPACE}
-if [${env.Westerwald}] then
-   cp ./ffwwsites/westerwald ./site -r
-   make update
-   make GLUON_TARGET=ar71xx-generic V=s -j4
+
+if ! [${Westerwald} = ''];
+   then
+      cp ./ffwwsites/westerwald ./site -r
+      make update
+      make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${env.Altenkirchen}] then
-   cp ./ffwwsites/altenkirchen ./site -r
-   make update
-   make GLUON_TARGET=ar71xx-generic V=s -j4
+if ! [${Altenkirchen} = ''];
+   then
+      cp ./ffwwsites/altenkirchen ./site -r
+      make update
+      make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${env.Limburg}] then
-   cp ./ffwwsites/limburg ./site -r
-   make update
-   make GLUON_TARGET=ar71xx-generic V=s -j4
+if ! [${Limburg} = ''];
+   then
+      cp ./ffwwsites/limburg ./site -r
+      make update
+      make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${env.Neuwied}] then
-   cp ./ffwwsites/neuwied ./site -r
-   make update
-   make GLUON_TARGET=ar71xx-generic V=s -j4
+if ! [${Neuwied} = ''];
+   then
+      cp ./ffwwsites/neuwied ./site -r
+      make update
+      make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
-if [${env.Testnetz}] then
-   cp ./ffwwsites/testnetz ./site -r
-   make update
-   make GLUON_TARGET=ar71xx-generic V=s -j4
+if ! [${Testnetz} = ''];
+   then
+      cp ./ffwwsites/testnetz ./site -r
+      make update
+      make GLUON_TARGET=ar71xx-generic V=s -j4
 fi
