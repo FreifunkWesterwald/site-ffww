c48561788b1d64e776e77c139e1981e380e683ae
Christian Paffhausen <thepaffy@thepaffy.de>
2016-10-14 14:12:03 +0200

    Added gluon-migrate-vpn to testnetz site

--- testnetz/site.mk
+++ testnetz/site.mk
@@ -23,6 +23,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-mesh-vpn-fastd \
 	gluon-mesh-vpn-tunneldigger \
 	gluon-tunneldigger-watchdog \
+	gluon-migrate-vpn \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
