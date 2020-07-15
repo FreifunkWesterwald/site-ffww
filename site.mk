79af537ecc3cfa350a126b827d94c68cb3dd10d0
Timo Widdau <git@twiddau.de>
2020-07-15 20:21:41 +0000

    Update site.mk and build.yml

--- site.mk
+++ site.mk
@@ -1,45 +1,63 @@
 ##	gluon site.mk makefile for Freifunk Westerwald
 
+##	GLUON_FEATURES
+#		Specify Gluon features/packages to enable;
+#		Gluon will automatically enable a set of packages
+#		depending on the combination of features listed
+
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	mesh-batman-adv-15 \
+	mesh-vpn-fastd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard
+
 ##	GLUON_SITE_PACKAGES
 #		specify gluon/openwrt packages to include here
-GLUON_SITE_PACKAGES := \
-	gluon-respondd \
-	gluon-autoupdater \
-	gluon-config-mode-core \
-	gluon-config-mode-autoupdater\
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-network \
-	gluon-web-wifi-config \
-	gluon-mesh-batman-adv-15 \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	iwinfo \
-	iptables \
-	haveged \
-	gluon-web-private-wifi \
-	gluon-banner \
-	gluon-ssid-changer
+GLUON_SITE_PACKAGES := iwinfo #\
+#	gluon-respondd \
+#	gluon-autoupdater \
+#	gluon-config-mode-core \
+#	gluon-config-mode-autoupdater\
+#	gluon-config-mode-hostname \
+#	gluon-config-mode-mesh-vpn \
+#	gluon-config-mode-geo-location \
+#	gluon-config-mode-contact-info \
+#	gluon-ebtables-filter-multicast \
+#	gluon-ebtables-filter-ra-dhcp \
+#	gluon-web-admin \
+#	gluon-web-autoupdater \
+#	gluon-web-network \
+#	gluon-web-wifi-config \
+#	gluon-mesh-batman-adv-15 \
+#	gluon-mesh-vpn-fastd \
+#	gluon-radvd \
+#	gluon-setup-mode \
+#	gluon-status-page \
+#	iwinfo \
+#	iptables \
+#	haveged \
+#	gluon-web-private-wifi \
+#	gluon-banner \
+#	gluon-ssid-changer
 
-ifeq ($(GLUON_TARGET),x86-generic)
+#ifeq ($(GLUON_TARGET),x86-generic)
 # support the usb stack on x86 devices
 # and add a few common USB NICs
-GLUON_SITE_PACKAGES += \
-	kmod-usb-core \
-	kmod-usb2 \
-	kmod-usb-hid \
-	kmod-usb-net \
-	kmod-usb-net-asix \
-	kmod-usb-net-dm9601-ether
-endif
+#GLUON_SITE_PACKAGES += \
+#	kmod-usb-core \
+#	kmod-usb2 \
+#	kmod-usb-hid \
+#	kmod-usb-net \
+#	kmod-usb-net-asix \
+#	kmod-usb-net-dm9601-ether
+#endif
+
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
 #		gluon relies on
@@ -69,6 +87,5 @@ GLUON_PRIORITY ?= 0
 # Languages to include
 GLUON_LANGS ?= en de
 
-GLUON_ATH10K_MESH ?= 11s
-
+#GLUON_ATH10K_MESH ?= 11s
 GLUON_DEPRECATED=upgrade
