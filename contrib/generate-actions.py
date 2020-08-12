206b3948891470b01f3a58e6e37604248c5302ce
pmenke <info@pmenke.de>
2020-08-12 11:01:15 +0200

    Build on-release instead of on-tag Removed "master-$(date)" from build version

--- contrib/generate-actions.py
+++ contrib/generate-actions.py
@@ -8,47 +8,19 @@ ACTIONS_HEAD = """
 # `contrib/list-targets.sh | contrib/generate-actions.py > .github/workflows/build-gluon.yml`
 name: Build Gluon
 on:
-  push:
-    tags:
-      - '*'
-jobs:
-  create-release:
-    runs-on: ubuntu-latest
-    outputs:
-      upload_url: ${{ steps.create_release.outputs.upload_url }}
-      tag_name: ${{ steps.tag_name.outputs.tag_name }}
-    steps:
-      - name: Compute tag name
-        id: tag_name
-        run: echo ::set-output name=tag_name::$(echo ${{ github.ref }} | sed 's!refs/tags/!!')
-        
-      - name: Check if this is an unstable release
-        id: check_unstable
-        run: |
-         if [[ ${{ steps.tag_name.outputs.tag_name }} =~ ^unstable ]]; then
-           echo ::set-output name=unstable_release::true
-         else 
-           echo ::set-output name=unstable_release::false
-         fi
-          
-      - name: Create Release
-        id: create_release
-        uses: actions/create-release@v1
-        env:
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        with:
-          tag_name: ${{ github.ref }}
-          release_name: Release ${{ steps.tag_name.outputs.tag_name }}
-          draft: false
-          prerelease: ${{ steps.check_unstable.outputs.unstable_release }}
+  release:
+    types:
+      - 'published'
 """
 
 ACTIONS_TARGET="""
   {target_name}:
     name: {target_name}
     runs-on: ubuntu-latest
-    needs: create-release
     steps:
+      - name: Compute tag name
+        id: tag_name
+        run: echo ::set-output name=tag_name::$(echo ${{ github.ref }} | sed 's!refs/tags/!!')
       
       - name: Checkout repository
         uses: actions/checkout@v2
@@ -59,9 +31,9 @@ ACTIONS_TARGET="""
         run: echo ::set-env name=GLUON_BRANCH::master
 
       - name: Set GLUON_RELEASE environment variable
-        run: echo ::set-env name=GLUON_RELEASE::${{BUILD_VERSION:-XX}}+${{GLUON_BRANCH:-master}}$(date '+%Y%m%d%H%M')
+        run: echo ::set-env name=GLUON_RELEASE::${{BUILD_VERSION:-XX}}
         env:
-          BUILD_VERSION: ${{{{ needs.create-release.outputs.tag_name }}}}
+          BUILD_VERSION: ${{{{ steps.tag_name.outputs.tag_name }}}}
 
       - name: Install apt Dependencies
         run: sudo contrib/actions/setup-dependencies.sh
@@ -69,12 +41,13 @@ ACTIONS_TARGET="""
       - name: Build
         run: contrib/actions/run-build.sh {target_name}
 
-      - name: Archive build logs
-        if: ${{{{ !cancelled() }}}}
-        uses: actions/upload-artifact@v2
-        with:
-          name: {target_name}_logs
-          path: gluon/openwrt/logs
+      #TODO: Auto-activate, when verbose logging is enabled?
+      #- name: Archive build logs
+      #  if: ${{{{ !cancelled() }}}}
+      #  uses: actions/upload-artifact@v2
+      #  with:
+      #    name: {target_name}_logs
+      #    path: gluon/openwrt/logs
 
       - name: Archive build output
         uses: actions/upload-artifact@v2
@@ -91,7 +64,7 @@ ACTIONS_TARGET="""
         env:
           GITHUB_TOKEN: ${{{{ secrets.GITHUB_TOKEN }}}}
         with:
-          upload_url: ${{{{ needs.create-release.outputs.upload_url }}}} 
+          upload_url: ${{{{ github.event.release.upload_url }}}} 
           asset_path: ./{target_name}.tar.xz
           asset_name: {target_name}.tar.xz
           asset_content_type: application/x-xz
