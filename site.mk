2335ba7893e2d639ecba5075d9a3309044a05ff4
loppermann <git@loppermann.de>
2020-08-18 12:11:20 +0200

    edit gluon packages

--- site.mk
+++ site.mk
@@ -21,34 +21,154 @@ GLUON_FEATURES := \
 ##	GLUON_SITE_PACKAGES
 #		specify gluon/openwrt packages to include here
 GLUON_SITE_PACKAGES :=\
-#	gluon-respondd \
 	gluon-autoupdater \
 	ffffm-button-bind \
 	gluon-autoupdater-branch-fix \
 	gluon-config-mode-geo-location-osm \
-#	gluon-config-mode-core \
-#	gluon-config-mode-autoupdater\
-#	gluon-config-mode-hostname \
-#	gluon-config-mode-mesh-vpn \
-#	gluon-config-mode-geo-location \
-#	gluon-config-mode-contact-info \
-#	gluon-ebtables-filter-multicast \
-#	gluon-ebtables-filter-ra-dhcp \
-#	gluon-web-admin \
-#	gluon-web-autoupdater \
-#	gluon-web-network \
-#	gluon-web-wifi-config \
-#	gluon-mesh-batman-adv-15 \
-#	gluon-mesh-vpn-fastd \
-#	gluon-radvd \
-#	gluon-setup-mode \
+	gluon-ebtables-filter-multicast \
+    gluon-ebtables-filter-ra-dhcp \
+    gluon-ebtables-source-filter \
+    gluon-radv-filterd \
+    gluon-respondd \
+	gluon-setup-mode \
 	gluon-status-page \
-#	iwinfo \
-#	iptables \
-#	haveged \
+	iwinfo \
+	iptables \
 	gluon-web-private-wifi \
 	gluon-banner \
-	gluon-ssid-changer
+	gluon-ssid-changer \
+	respondd-module-airtime
+#############################
+# Additional package sets
+#############################
+
+# USB Human Interface
+USB_PACKAGES_HID := \
+	kmod-usb-hid \
+	kmod-hid-generic
+
+# USB Serial
+USB_PACKAGES_SERIAL := \
+	kmod-usb-serial \
+	kmod-usb-serial-ftdi \
+	kmod-usb-serial-pl2303
+
+# USB Storage
+USB_PACKAGES_STORAGE := \
+	block-mount \
+	blkid \
+	kmod-fs-ext4 \
+	kmod-fs-ntfs \
+	kmod-fs-vfat \
+	kmod-usb-storage \
+	kmod-usb-storage-extras \
+	kmod-nls-base \
+	kmod-nls-cp1250 \
+	kmod-nls-cp1251 \
+	kmod-nls-cp437 \
+	kmod-nls-cp850 \
+	kmod-nls-cp852 \
+	kmod-nls-iso8859-1 \
+	kmod-nls-iso8859-13 \
+	kmod-nls-iso8859-15 \
+	kmod-nls-iso8859-2 \
+	kmod-nls-utf8 \
+swap-utils
+
+# USB Network
+USB_PACKAGES_NET := \
+	kmod-ath9k-htc \
+	kmod-rt2800-usb \
+	kmod-mii \
+	kmod-nls-base \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-eem \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-cdc-mbim \
+	kmod-usb-net-cdc-subset \
+	kmod-usb-net-dm9601-ether \
+	kmod-usb-net-hso \
+	kmod-usb-net-huawei-cdc-ncm \
+	kmod-usb-net-ipheth \
+	kmod-usb-net-kalmia \
+	kmod-usb-net-kaweth \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-qmi-wwan \
+	kmod-usb-net-rndis \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-sierrawireless \
+	kmod-usb-net-smsc95xx
+
+# PCI-Express Network
+PCIE_PACKAGES_NET := \
+	kmod-bnx2
+
+# additional packages
+TOOLS_PACKAGES := \
+	iperf \
+	socat \
+	tcpdump \
+	vnstat
+
+# Group previous package sets
+USB_PACKAGES_WITHOUT_HID := \
+	usbutils \
+	usb-modeswitch \
+	$(USB_PACKAGES_SERIAL) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET)
+
+USB_PACKAGES := \
+	$(USB_PACKAGES_HID) \
+	$(USB_PACKAGES_WITHOUT_HID)
+
+PCIE_PACKAGES := \
+	pciutils \
+	$(PCIE_PACKAGES_NET)
+##################################
+# Assign package sets to targets
+##################################
+
+# Embedded Routers
+ifeq ($(GLUON_TARGET),ar71xx-generic)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES_WITHOUT_HID)
+endif
+
+ifeq ($(GLUON_TARGET),ar71xx-nand)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES_WITHOUT_HID)
+endif
+
+ifeq ($(GLUON_TARGET),mpc85xx-generic)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES_WITHOUT_HID)
+endif
+
+# x86 Generic Purpose Hardware
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES) $(PCIE_PACKAGES) $(TOOLS_PACKAGES)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES) $(PCIE_PACKAGES) $(TOOLS_PACKAGES)
+endif
+
+# PCEngines ALIX Boards
+ifeq ($(GLUON_TARGET),x86-geode)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES) $(PCIE_PACKAGES) $(TOOLS_PACKAGES)
+endif
+
+#  Raspberry Pi A/B/B+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES)
+endif
+
+# Raspberry Pi 2
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+GLUON_SITE_PACKAGES += $(USB_PACKAGES)
+endif
+
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
