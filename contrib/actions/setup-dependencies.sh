eeae3dd8579cf0b5905658854220ab9627ba864c
pmenke <info@pmenke.de>
2021-08-17 15:43:46 +0200

    added new build dependencies from gluon `setup-dependencies.sh`

--- contrib/actions/setup-dependencies.sh
+++ contrib/actions/setup-dependencies.sh
@@ -2,4 +2,5 @@
 set -e
 
 apt-get update
-apt-get install -y sudo python git make gcc g++ unzip libncurses5-dev zlib1g-dev subversion gawk bzip2 libssl-dev wget
+apt-get install -y sudo python3 git make gcc g++ unzip libncurses5-dev zlib1g-dev subversion gawk bzip2 libssl-dev \
+                   wget rsync file
