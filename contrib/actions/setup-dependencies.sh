ab07f50a6036c30b63844fa9e90c15eb879178e6
loppermann <git@loppermann.de>
2022-09-30 17:45:09 +0200

    add dependencies for x86 image

--- contrib/actions/setup-dependencies.sh
+++ contrib/actions/setup-dependencies.sh
@@ -2,4 +2,4 @@
 set -e
 
 apt-get update
-apt-get install -y sudo git make gcc g++ unzip libncurses5-dev zlib1g-dev subversion gawk bzip2 libssl-dev wget python3 rsync file
+apt-get install -y sudo git make gcc g++ unzip libncurses5-dev zlib1g-dev subversion gawk bzip2 libssl-dev wget python3 rsync file qemu-utils
